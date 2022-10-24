2c2ae31c7022a4aad4d48577d42c7129f2966f12
Steven Sisavath <stevenlsisavath@gmail.com>
2022-10-24 16:27:56 -0500

    Created function(run_day_trip_generator) to generate a random day trip

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -3,10 +3,6 @@ restaurant_list = ['LongHorn Steakhouse', 'Outback Steakhouse', 'Saltgrass Steak
 transportation_list = ['Hovercraft', 'Bus', 'Personal Vehicle', 'Uber']
 entertainment_list = ['Amusement Park', 'Hiking', 'Circus', 'Arcade']
 list_of_lists = [destination_list, restaurant_list, transportation_list, entertainment_list]
-destination_updated = ''
-restaurant_updated = ''
-transportation_updated = ''
-entertainment_updated = ''
 import random
 
 def generate_random_item(list): #generate a random item from a list of 4 strings
@@ -21,33 +17,40 @@ def generate_random_item(list): #generate a random item from a list of 4 strings
         return list[3]
 
 #test
-destination_item = generate_random_item(destination_list)
-restaurant_item = generate_random_item(restaurant_list)
-transportation_item = generate_random_item(transportation_list)
-entertainment_item = generate_random_item(entertainment_list)
-print(destination_item)
+# destination_item = generate_random_item(destination_list)
+# print(destination_item)
 
 def print_full_trip(lists): #only function is to run in beginning of final function call
     destination = generate_random_item(destination_list)
     restaurant = generate_random_item(restaurant_list)
     transportation = generate_random_item(transportation_list)
     entertainment = generate_random_item(entertainment_list)
-    destination = destination_updated
-    restaurant = restaurant_updated
-    transportation = transportation_updated
-    entertainment = entertainment_updated
     for element in range(len(lists)):
-            print(f'Destination: {destination}')
-            print(f'Restaurant: {restaurant}')
-            print(f'Transportation: {transportation}')
-            print(f'Entertainment: {entertainment}')
-            return
+        # print('Here is a randomly generated day trip!')
+        # print(f'Destination: {destination}')
+        # print(f'Restaurant: {restaurant}')
+        # print(f'Transportation: {transportation}')
+        # print(f'Entertainment: {entertainment}')
+        return destination, restaurant, transportation, entertainment
 
-print_full_trip(list_of_lists)
+destination, restaurant, transportation, entertainment = print_full_trip(list_of_lists)
+# print(destination)         
 
-def validate_item():
+#test
+# print_full_trip(list_of_lists)
+
+def validate_items():
     valid_item = False
     user_input = ''
+    final_destination = destination
+    final_restaurant = restaurant
+    final_transportation = transportation
+    final_entertainment = entertainment
+    print('Here is a randomly generated day trip!')
+    print(f'Destination: {destination}')
+    print(f'Restaurant: {restaurant}')
+    print(f'Transportation: {transportation}')
+    print(f'Entertainment: {entertainment}')
     while valid_item == False:
         user_input_response = input('''
 Are you satisfied with all your options? 
@@ -61,70 +64,54 @@ destination
 restaurant
 transportation
 entertainment
-all
 ''')
             if user_input == 'destination' :
                 new_destination_item = generate_random_item(destination_list)
-                new_destination_item = destination_updated
                 print(f'New Destination: {new_destination_item}')
-            elif user_input == 'destination' :
+                final_destination = new_destination_item
+                valid_item = False
+            elif user_input == 'restaurant' :
                 new_restaurant_item = generate_random_item(restaurant_list)
-                new_restaurant_item = restaurant_updated
-                print(f'New Destination: {new_restaurant_item}')
-            elif user_input == 'destination' :
+                print(f'New Restaurant: {new_restaurant_item}')
+                final_restaurant = new_restaurant_item
+                valid_item = False
+            elif user_input == 'transportation' :
                 new_transportation_item = generate_random_item(transportation_list)
-                new_transportation_item = transportation_updated
-                print(f'New Destination: {new_transportation_item}')
-            elif user_input == 'destination' :
+                print(f'New Transportation: {new_transportation_item}')
+                final_transportation = new_transportation_item
+                valid_item = False
+            elif user_input == 'entertainment' :
                 new_entertainment_item = generate_random_item(entertainment_list)
-                new_entertainment_item = entertainment_updated
-                print(f'New Destination: {new_entertainment_item}')
+                print(f'New Entertainment: {new_entertainment_item}')
+                final_entertainment = new_entertainment_item
+                valid_item = False
         elif user_input_response == 'yes' :
+            print(f'''
+Great! Here is your final result!
+
+Destination: {final_destination}
+Restaurant: {final_restaurant}
+Transportation: {final_transportation}
+Entertainment: {final_entertainment}
+
+Have an amazing time on your day trip!
+        ''')
             valid_item = True
-        elif user_input_response == 'yes' or 'no':
+            return valid_item
+        else:
             valid_item = False
-    return valid_item
+    return destination, restaurant, transportation, entertainment
 
-validated_item = validate_item()
-print(validated_item)
+# validated_item = validate_items()
+# print(validated_item)
 
-# def run_day_trip_generator():
+def run_day_trip_generator():
+    print_full_trip(list_of_lists)
+    validate_items()
+
+run_day_trip_generator()
 
 
 # def run_day_trip_generator():
 #     print_full_trip(list_of_lists)
 #     validate_item()
-
-
-# # ---------------------------------------------------------------
-# print_full_trip(list_of_lists)
-
-# def run_day_trip_generator():
-#     user_input = True
-#     no = True
-#     yes = True
-#     print_full_trip(list_of_lists)
-#     input('Are you satisfied with your trip? yes/no')
-#     if user_input == yes :
-#         print_full_trip
-    
-    
-# run_day_trip_generator
-
-
-
-# def determine_satisfaction():
-#     satisfied = 'yes'
-#     not_satisfied = 'no'
-#     input('Are you satisfied with your results? yes or no')
-#     if satisfied == 'yes' :
-#         print(f'''
-# Great, please enjoy your day trip! Here are your final results: {print(print_full_trip(list_of_lists))}
-#         ''')
-#         return ()
-    # elif not_satisfied == 'no' :
-    #     print()
-    # else :
-    #     if satisfied != 'yes' and not_satisfied != 'no' :
-
-# determine_satisfaction()
